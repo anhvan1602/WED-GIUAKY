bc1aa1da3e636d6f8f1ea1c042ed0293e07830db
anhvan1602 <72864584+anhvan1602@users.noreply.github.com>
2021-04-25 16:00:04 +0700

    commit2

--- main.js
+++ main.js
@@ -215,64 +215,17 @@ function carousel() {
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 //------FIND-------------
-/* This script and many more are available free online at
-The JavaScript Source :: http://javascript.internet.com
-Created by: Stuart Langridge :: http://www.kryogenix.org/ */
 
-// Licenced under the MIT licence
 
-function highlightWord(node,word) {
-  // Iterate into this nodes childNodes
-  if (node.hasChildNodes) {
-    var hi_cn;
-    for (hi_cn=0;hi_cn<node.childNodes.length;hi_cn++) {
-      highlightWord(node.childNodes[hi_cn],word);
-    }
-  }
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1000,
+  reset: true
+});
 
-  // And do this node itself
-  if (node.nodeType == 3) { // text node
-    tempNodeVal = node.nodeValue.toLowerCase();
-    tempWordVal = word.toLowerCase();
-    if (tempNodeVal.indexOf(tempWordVal) != -1) {
-      pn = node.parentNode;
-      if (pn.className != "searchword") {
-        // word has not already been highlighted!
-        nv = node.nodeValue;
-        ni = tempNodeVal.indexOf(tempWordVal);
-        // Create a load of replacement nodes
-        before = document.createTextNode(nv.substr(0,ni));
-        docWordVal = nv.substr(ni,word.length);
-        after = document.createTextNode(nv.substr(ni+word.length));
-        hiwordtext = document.createTextNode(docWordVal);
-        hiword = document.createElement("span");
-        hiword.className = "searchword";
-        hiword.appendChild(hiwordtext);
-        pn.insertBefore(before,node);
-        pn.insertBefore(hiword,node);
-        pn.insertBefore(after,node);
-        pn.removeChild(node);
-      }
-    }
-  }
-}
-
-function googleSearchHighlight() {
-  if (!document.createElement) return;
-  ref = document.referrer;
-  if (ref.indexOf('?') == -1) return;
-  qs = ref.substr(ref.indexOf('?')+1);
-  qsa = qs.split('&');
-  for (i=0;i<qsa.length;i++) {
-    qsip = qsa[i].split('=');
-    if (qsip.length == 1) continue;
-    if (qsip[0] == 'q' || qsip[0] == 'p') { // q= for Google, p= for Yahoo
-      words = unescape(qsip[1].replace(/\+/g,' ')).split(/\s+/);
-      for (w=0;w<words.length;w++) {
-         highlightWord(document.getElementsByTagName("body")[0],words[w]);
-      }
-    }
-  }
-}
-
-window.onload = googleSearchHighlight;
+sr.reveal(`.section, .section-subtitle,
+           .menu__img, .menu, .product1`, {
+  interval: 200
+})
